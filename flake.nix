fe74eca5262ee7bd18b712ae0d2de39a5bfc4745
Matko <matkohrad@gmail.com>
2026-09-26 18:25:19 +0200

    Bump version to 0.2.14 for swell envelope fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.13";
+          version = "0.2.14";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
