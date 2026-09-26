8c75ce76c1fa65d692027642225097fc1cee6ee7
Matko <matkohrad@gmail.com>
2026-09-26 19:03:52 +0200

    Bump version to 0.2.16 after splat revert

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.15";
+          version = "0.2.16";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
