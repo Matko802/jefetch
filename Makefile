587fa250174d3fd5f5047eff59d6ed080aaffe70
Matko <matkohrad@gmail.com>
2026-09-26 19:19:57 +0200

    Bump version to 0.2.17 for cloud densification fix

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION ?= 0.2.16
+VERSION ?= 0.2.17
 PREFIX ?= /usr/local
 CC ?= cc
 CFLAGS ?= -O2
--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.16";
+          version = "0.2.17";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
