0edab510ed130e48ee6ab6be39bc0f8f9cbb77a2
Matko <matkohrad@gmail.com>
2026-09-26 21:39:07 +0200

    Bump version to 0.2.21 for signal parity

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION ?= 0.2.20
+VERSION ?= 0.2.21
 PREFIX ?= /usr/local
 CC ?= cc
 CFLAGS ?= -O2
--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.20";
+          version = "0.2.21";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
