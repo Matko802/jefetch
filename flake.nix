aba55fff73d8a17210ad5e9c66cec80e3e6a0e0c
Matko <matkohrad@gmail.com>
2026-09-26 15:37:05 +0200

    Bump version to 0.2.12 for scale footprint fill fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.11";
+          version = "0.2.12";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
