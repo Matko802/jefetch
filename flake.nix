45bcbe1c26497a0032202bbb89c85a48d90406be
Matko <matkohrad@gmail.com>
2026-09-26 18:54:16 +0200

    Bump version to 0.2.15 for exact-zero rest snap fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.14";
+          version = "0.2.15";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
