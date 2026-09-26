eabf71114f746614554f1a9bcdf825ab187f41f4
Matko <matkohrad@gmail.com>
2026-09-26 20:48:38 +0200

    Bump version to 0.2.20 for speed sensitivity

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         { pkgs }:
         pkgs.stdenv.mkDerivation {
           pname = "jefetch";
-          version = "0.2.19";
+          version = "0.2.20";
           src = pkgs.lib.cleanSource ./.;
 
           nativeBuildInputs = [ pkgs.gnumake ];
